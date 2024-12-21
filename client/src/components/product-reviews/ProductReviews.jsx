af597819b96e733eb314d6e55e8044201355a23c
velizar-velikov <vvelikov7.22@gmail.com>
2024-12-21 22:27:39 +0200

    add back button for reviews section

--- client/src/components/product-reviews/ProductReviews.jsx
+++ client/src/components/product-reviews/ProductReviews.jsx
@@ -1,10 +1,16 @@
-import { Container } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import Accordion from 'react-bootstrap/Accordion';
+import { Link } from 'react-router-dom';
 
 export default function ProductReviews() {
     return (
-        <Container className="container-sm col-8 col-md-7 col-lg-5 mt-5 p-4 p-lg-5 bg-dark-subtle shadow rounded-3">
-            <h3 className="mb-3 text-center">Sports t-shirt reviews</h3>
+        <Container className="container-sm col-8 col-md-7 col-lg-6 mt-5 p-4 p-lg-5 bg-dark-subtle shadow rounded-3">
+            <div className="d-flex  mb-3">
+                <Button as={Link} to="/catalog/:id/details" className="col-2 h-50">
+                    Back
+                </Button>
+                <h3 className="col-6 col-md-8 text-center">Sports t-shirt reviews</h3>
+            </div>
             <Accordion defaultActiveKey="0">
                 <Accordion.Item eventKey="0">
                     <Accordion.Header>
