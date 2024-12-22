94e9f74ce403945bbcafeb272cccf138b109242b
velizar-velikov <vvelikov7.22@gmail.com>
2024-12-22 10:15:24 +0200

    adjust create form inputs

--- client/src/components/create/Create.jsx
+++ client/src/components/create/Create.jsx
@@ -1,24 +1,52 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import { Col, Row } from 'react-bootstrap';
 
 export default function Create() {
     return (
-        <Container className="container-sm col-8 col-md-7 col-lg-5 mt-5 p-4 p-lg-5 bg-dark-subtle shadow rounded-3">
+        <Container className="container-sm col-8 col-md-7 col-lg-5 mt-5 mb-4 p-4 p-lg-5 bg-dark-subtle shadow rounded-3">
             <Form>
-                <h2>Create</h2>
+                <h2>Add product</h2>
+
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                    <Form.Label>Product name</Form.Label>
+                    <Form.Label>Name</Form.Label>
                     <Form.Control type="text" placeholder="Short sleeve t-shirt" />
                 </Form.Group>
+                <Row className="d-flex">
+                    <Col>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Brand</Form.Label>
+                            <Form.Control type="text" placeholder="Nike" />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+                            <Form.Label>Price</Form.Label>
+                            <Form.Control type="number" />
+                        </Form.Group>
+                    </Col>
+                </Row>
+
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-                    <Form.Label>Product summary</Form.Label>
-                    <Form.Control type="text" placeholder="comfortable t-shirt" />
+                    <Form.Label>Image URL</Form.Label>
+                    <Form.Control type="text" placeholder="https://..." />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+                    <Form.Label>Summary (up to 40 characters)</Form.Label>
+                    <Form.Control type="text" placeholder="comfortable cotton t-shirt" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
                     <Form.Label>Full description</Form.Label>
-                    <Form.Control type="text" placeholder="Great for summer and spring and also..." />
+                    <Form.Control as="textarea" rows={3} placeholder="Great for summer and spring and also..." />
                 </Form.Group>
+                <p className="small">
+                    <span className="d-inline-block mr-3">Note:</span>
+                    <span className="d-inline-block">
+                        Product will be added with stock of 1 from each size ( S, M, L ). If you want to add in stock you will be
+                        able to do it from product description after you have created the product.
+                    </span>
+                </p>
                 <Button variant="btn btn-dark mt-3 btn-outline-tertiary" type="submit">
                     Create
                 </Button>
