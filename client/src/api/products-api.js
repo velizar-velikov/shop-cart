928281a5f69a212f3e024a12a5ca5e4c759e3981
velizar-velikov <vvelikov7.22@gmail.com>
2024-12-22 14:25:32 +0200

    add products api

---
+++ client/src/api/products-api.js
@@ -0,0 +1,36 @@
+import requester from './request.js';
+
+const host = 'http://localhost:3030';
+
+const endpoints = {
+    all: '/data/products',
+    byId: (id) => `/data/products/${id}`,
+};
+
+function getAll() {
+    return requester.get(host + endpoints.all);
+}
+
+function getOne(productId) {
+    return requester.get(host + endpoints.byId(productId));
+}
+
+function create(data) {
+    return requester.post(host + endpoints.all, data);
+}
+
+function editById(productId, data) {
+    return requester.put(host + endpoints.byId(productId), data);
+}
+
+function deleteById(productId) {
+    return requester.delete(host + endpoints.byId(productId));
+}
+
+const productsAPI = {
+    getAll,
+    getOne,
+    create,
+    editById,
+    deleteById,
+};
