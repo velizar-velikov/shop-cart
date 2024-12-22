0fcb54dd5f53b75e2795a6cf7ef979b241c8c759
velizar-velikov <vvelikov7.22@gmail.com>
2024-12-22 10:16:11 +0200

    adjust register inputs

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -2,16 +2,27 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import { Link } from 'react-router-dom';
+import { Col, Row } from 'react-bootstrap';
 
 export default function Register() {
     return (
         <Container className="container-sm col-8 col-md-7 col-lg-5 mt-5 p-4 p-lg-5 pb-1 pb-lg-2 bg-dark-subtle shadow rounded-3">
             <Form>
                 <h2>Register</h2>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" placeholder="Peter" />
-                </Form.Group>
+                <Row className="d-flex">
+                    <Col>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>First name</Form.Label>
+                            <Form.Control type="text" placeholder="Peter" />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Last name</Form.Label>
+                            <Form.Control type="text" placeholder="Petrov" />
+                        </Form.Group>
+                    </Col>
+                </Row>
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="name@example.com" />
