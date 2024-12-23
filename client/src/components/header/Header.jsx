da63fa95b32c1a20eb49df6dfe9b16cfed6e5fe4
velizar-velikov <vvelikov7.22@gmail.com>
2024-12-23 12:33:39 +0200

    implement dynamic navigation

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -2,8 +2,10 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import { useAuthContext } from '../../contexts/AuthContext.jsx';
 
 export default function Header() {
+    const { isAuthenticated } = useAuthContext();
     return (
         <Navbar className="p-0" bg="dark" data-bs-theme="dark">
             <Container>
@@ -17,21 +19,28 @@ export default function Header() {
                     <Nav.Link as={Link} to="/catalog">
                         Catalog
                     </Nav.Link>
-                    <Nav.Link as={Link} to="/create">
-                        Create product
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="/login">
-                        Login
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="/register">
-                        Register
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="/logout">
-                        Logout
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="/cart" className="mx-2 mt-1 px-1 fa-lg">
-                        <i className="fa-solid fa-cart-shopping"></i>
-                    </Nav.Link>
+                    {isAuthenticated ? (
+                        <>
+                            <Nav.Link as={Link} to="/create">
+                                Create product
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/logout">
+                                Logout
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/cart" className="mx-2 mt-1 px-1 fa-lg">
+                                <i className="fa-solid fa-cart-shopping"></i>
+                            </Nav.Link>
+                        </>
+                    ) : (
+                        <>
+                            <Nav.Link as={Link} to="/login">
+                                Login
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/register">
+                                Register
+                            </Nav.Link>
+                        </>
+                    )}
                 </Nav>
             </Container>
         </Navbar>
