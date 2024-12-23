347b3da954ab1e83183dabbd3a7540a23afbebae
velizar-velikov <vvelikov7.22@gmail.com>
2024-12-23 10:49:26 +0200

    add authentication hooks to make api calls and change auth state

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,49 @@
+import authAPI from '../api/auth-api.js';
+import { useAuthContext } from '../contexts/AuthContext.jsx';
+
+export function useLogin() {
+    const { changeAuthState } = useAuthContext();
+
+    const loginHandler = async (email, password) => {
+        const result = await authAPI.login(email, password);
+        changeAuthState({
+            userId: result._id,
+            firstName: result.firstName,
+            email: result.email,
+            accessToken: result.accessToken,
+        });
+
+        return result;
+    };
+
+    return loginHandler;
+}
+
+export function useRegister() {
+    const { changeAuthState } = useAuthContext();
+
+    const registerHandler = async (firstName, lastName, email, password) => {
+        const result = await authAPI.register(firstName, lastName, email, password);
+        changeAuthState({
+            userId: result._id,
+            firstName: result.firstName,
+            email: result.email,
+            accessToken: result.accessToken,
+        });
+
+        return result;
+    };
+
+    return registerHandler;
+}
+
+export function useLogout() {
+    const { changeAuthState } = useAuthContext();
+
+    const logoutHandler = async () => {
+        await authAPI.logout();
+        changeAuthState(null);
+    };
+
+    return logoutHandler;
+}
