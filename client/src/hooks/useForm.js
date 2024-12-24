479c6be9430a210191cf0e5d70b07c9047c6d1a1
velizar-velikov <vvelikov7.22@gmail.com>
2024-12-24 12:02:20 +0200

    add useForm hook support for edit functionality

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,7 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function useForm(initialValues, submitCallback) {
     const [values, setValues] = useState(initialValues);
+
+    useEffect(() => {
+        setValues(initialValues);
+    }, [initialValues]);
 
     const changeHandler = (event) => {
         setValues((oldValues) => ({
