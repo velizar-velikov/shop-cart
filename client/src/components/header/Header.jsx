b0b23dfb1d807051e6773247edf0f661c16f4843
velizar-velikov <vvelikov7.22@gmail.com>
2024-12-28 23:17:04 +0200

    add cart quantity bubble

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -28,8 +28,15 @@ export default function Header() {
                             <Nav.Link as={Link} to="/logout">
                                 Logout
                             </Nav.Link>
-                            <Nav.Link as={Link} to="/cart" className="mx-2 mt-1 px-1 fa-lg">
-                                <i className="fa-solid fa-cart-shopping"></i>
+                            <Nav.Link as={Link} to="/cart" className="position-relative  mx-2 mt-1 px-1">
+                                <i className="fa-solid fa-cart-shopping fa-xl"></i>
+                                {/* overide boostrap styles with inline css */}
+                                <span
+                                    style={{ fontSize: '0.85rem', padding: '0.04rem 0.25rem 0.15rem 0.25rem' }}
+                                    className="position-absolute top-25 start-75 translate-middle badge rounded-circle bg-danger py-0.125 px-0.25"
+                                >
+                                    3
+                                </span>
                             </Nav.Link>
                         </>
                     ) : (
--- readme.md
+++ readme.md
@@ -1,9 +1,5 @@
 # Plan
 
-### 3.2 Edit review functionality from section reviews
-
-### 3.3 Delete review functionality from section reviews
-
 ## 6. Catalog
 
 ### 6.1 Search by category and product - [x]
@@ -15,3 +11,9 @@
 ### 6.2 Pagination should work with the found search [x]
 
 ### 6.2 Connect the category, search and current page with the URL
+
+## 7. Checkout
+
+### 7.1 Cart - items that were in stock in time of adding to cart but are now out of stock or with not enough quantity should have a message to inform user
+
+### 7.2 Cart -> Next step -> add items from cart for checkout only if it is not out of stock or if it is not a higher quantity than the in stock quantity
