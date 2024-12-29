0a3e5e6dd3d56c2d63897f14635d7c38fb570c1a
velizar-velikov <vvelikov7.22@gmail.com>
2024-12-29 13:01:20 +0200

    change product deletion to be with inactive flag instead of actual record delete

--- client/src/api/products-api.js
+++ client/src/api/products-api.js
@@ -9,6 +9,7 @@ const endpoints = {
 
 async function getAll() {
     const urlParams = new URLSearchParams({
+        where: 'inactive=false',
         sortBy: `_createdOn%20desc`,
     });
     const url = new URL(host + endpoints.all);
@@ -22,7 +23,7 @@ async function getCatalogProducts(currentPage, search) {
     const category_search_string = search.category == 'All categories' ? '' : search.category;
 
     const urlParams = new URLSearchParams({
-        where: `category LIKE "${category_search_string}" AND name LIKE "${search.name}"`,
+        where: `category LIKE "${category_search_string}" AND name LIKE "${search.name}" AND inactive=false`,
         offset: ((currentPage - 1) * pageSize).toString(),
         pageSize: pageSize.toString(),
     });
@@ -38,13 +39,16 @@ async function getCatalogProducts(currentPage, search) {
 
 function getLatest(productsCount) {
     const urlParams = new URLSearchParams({
-        sortBy: '_createdOn%20desc',
+        where: 'inactive=false',
         offset: '0',
         pageSize: productsCount,
     });
 
-    const url = new URL(host + endpoints.all);
-    url.search = decodeURIComponent(urlParams);
+    const urlParamSort = new URLSearchParams({
+        sortBy: '_createdOn%20desc',
+    });
+
+    const url = `${host}${endpoints.all}?${urlParams.toString()}&${decodeURIComponent(urlParamSort)}`;
 
     return requester.get(url);
 }
@@ -54,19 +58,26 @@ function getOne(productId) {
 }
 
 function create(data) {
-    return requester.post(host + endpoints.all, data);
+    return requester.post(host + endpoints.all, { ...data, inactive: false });
 }
 
 function editById(productId, data) {
-    return requester.put(host + endpoints.byId(productId), data);
+    return requester.patch(host + endpoints.byId(productId), data);
 }
 
+/**
+ * The request only marks the product as inactive
+ * as other collections (cart, orders, ...) reference it by relation
+ * @param {string} productId
+ * @returns
+ */
 function deleteById(productId) {
-    return requester.delete(host + endpoints.byId(productId));
+    return requester.patch(host + endpoints.byId(productId), { inactive: true });
 }
 
 function getDistinctCategories() {
     const urlParams = new URLSearchParams({
+        where: 'inactive=false',
         distinct: 'category',
         select: 'category',
     });
