60b43912c1f77a85590ff1d004c4e876570b4cda
velizar-velikov <vvelikov7.22@gmail.com>
2025-01-02 12:55:42 +0200

    correct classNames

--- client/src/components/not-found/NotFound.jsx
+++ client/src/components/not-found/NotFound.jsx
@@ -3,13 +3,13 @@ import paths from '../../config/paths.js';
 
 export default function NotFound() {
     return (
-        <div class="text-dark">
-            <div class="d-flex align-items-center justify-content-center min-vh-100 px-2">
-                <div class="text-center">
-                    <h1 class="display-1 fw-bold">404</h1>
-                    <p class="fs-2 fw-medium mt-4">Oops! Page not found</p>
-                    <p class="mt-4 mb-5">The page you're looking for doesn't exist or has been moved.</p>
-                    <Link to={paths.home.path} class="btn btn-dark px-4 py-2">
+        <div className="text-dark">
+            <div className="d-flex align-items-center justify-content-center min-vh-100 px-2">
+                <div className="text-center">
+                    <h1 className="display-1 fw-bold">404</h1>
+                    <p className="fs-2 fw-medium mt-4">Oops! Page not found</p>
+                    <p className="mt-4 mb-5">The page you're looking for doesn't exist or has been moved.</p>
+                    <Link to={paths.home.path} className="btn btn-dark px-4 py-2">
                         Go Home
                     </Link>
                 </div>
